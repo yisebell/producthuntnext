680d610a1e029404687e8063561a45f199475423
INDRA\ljhidalgo <ljhidalgo@indracompany.com>
2021-04-15 19:29:40 -0500

    estilos formulario crear-cuenta

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,11 +1,51 @@
 import React from 'react';
+import { css } from '@emotion/react';
 import Layout from '../components/layouts/Layout';
+import { Formulario, Campo, InputSubmit } from '../components/ui/Formulario';
 
 export default function CrearCuenta() {
   return (
     <div>
       <Layout>
-        <h1>CrearCuenta</h1>
+        <h1
+          css={css`
+            text-align: center;
+            margin-top: 5rem;
+          `}
+        >CrearCuenta</h1>
+        <Formulario>
+          <Campo>
+            <label htmlFor="nombre">Nombre</label>
+            <input 
+              type="text"
+              id="nombre"
+              placeholder="Tu nombre"
+              name="nombre"
+            />
+          </Campo>
+          <Campo>
+            <label htmlFor="email">Email</label>
+            <input 
+              type="text"
+              id="email"
+              placeholder="Tu email"
+              name="email"
+            />
+          </Campo>
+          <Campo>
+            <label htmlFor="password">Password</label>
+            <input 
+              type="text"
+              id="password"
+              placeholder="Tu password"
+              name="password"
+            />
+          </Campo>
+          <InputSubmit 
+            type="submit"
+            value="Crear cuenta"
+          />
+        </Formulario>
       </Layout>      
     </div>
   )
